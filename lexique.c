309f8db151278f49431c0771e9e9a8bbdb05aaa5
ng <nicolas@nghs.fr>
2007-11-28 18:07:50 +0000

    travail sur lexique en cours ne compile pas la derniere revi qui compile est le r22
    git-svn-id: file:///Users/ng/svn-local/to_share/trad@23 efe719f2-5d0f-4ff0-a31d-317092ae7015

--- lexique.c
+++ lexique.c
@@ -1,6 +1,56 @@
 
 #include <string.h>
 #include "lexique.h"
+#include "assert.h"
+
+lexique_t * create_lexique()
+{
+    lexique_t * l = (lexique_t *)malloc(sizeof(lexique_t));
+
+    c_assert2(l, "malloc failed");
+
+    l->size = 50;
+    l->table = (char**)malloc(l->size * sizeof(char*));
+    l->hashtbl = create_hashtable(50, lexique_str_hash, lexique_str_equals);
+
+    c_assert2(l->table, "malloc failed");
+    c_assert(l->hashtbl);
+
+    return l;
+}
+
+void free_lexique(lexique_t * l)
+{
+    if(!l) return;
+
+    free(l->table);
+    hashtable_destroy(l->hashtbl, 1);
+    free(l);
+}
+
+void lexique_add(lexique_t * l, char * str)
+{
+    c_assert(l && str);
+}
+
+int lexique_search(lexique_t * l, char * str)
+{
+    c_assert(l && str);
+}
+
+char* lexique_get(lexique_t * l, size_t index)
+{
+    c_assert(l);
+    c_assert(index >= 0 && index < lexique_count(l));
+    return l->table[index];
+}
+
+size_t lexique_count(lexique_t * l)
+{
+    c_assert(l);
+    c_assert(l->size == hashtable_count(l->hashtbl));
+    return l->size;
+}
 
 unsigned long lexique_str_hash(void * v)
 {
--- lexique.h
+++ lexique.h
@@ -4,7 +4,28 @@
 
 #include "hashtable/hashtable.h"
 
+DEFINE_HASHTABLE_INSERT(_lexique_insert, char*, size_t);
+DEFINE_HASHTABLE_SEARCH(_lexique_search, char*, size_t);
+DEFINE_HASHTABLE_REMOVE(_lexique_remove, char*, size_t);
 
+typedef struct
+{
+    /** Pour une recherche efficace*/
+    struct hashtable * hash;
+
+    /** Pour un accès rapide */
+    char ** tabletbl;
+    size_t size;
+} lexique_t;
+
+
+lexique_t * create_lexique();
+void free_lexique(lexique_t * l);
+
+void lexique_add(lexique_t * l, char * str);
+int lexique_search(lexique_t * l, char * str);
+char* lexique_get(lexique_t * l, size_t index);
+size_t lexique_count(lexique_t * l);
 
 unsigned long lexique_str_hash(void * str);
 int lexique_str_equals(void * k1, void * k2);
